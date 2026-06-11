e69edb23fee73dfd9740cafafbde3f2b628a7b99
olita16 <119816415+olita16@users.noreply.github.com>
2026-06-11 12:18:40 +0200

    final deployment + screenshots

--- eks/main.tf
+++ eks/main.tf
@@ -1,5 +1,6 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
+
   config = {
     bucket = var.state_bucket
     key    = "vpc/terraform.tfstate"
@@ -12,31 +13,41 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.29"
-  
+  cluster_version = "1.30"
+
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
   enable_cluster_creator_admin_permissions = true
 
-  vpc_id     = data.terraform_remote_state.vpc.outputs.vpc_id
-  subnet_ids  = data.terraform_remote_state.vpc.outputs.private_subnets
+  vpc_id    = data.terraform_remote_state.vpc.outputs.vpc_id
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
 
   eks_managed_node_groups = {
     cpu_group = {
       instance_types = ["t3.micro"]
-      ami_type = "AL2_x86_64"
-      desired_size   = 1
-      min_size       = 1
-      max_size       = 2
+      ami_type       = "AL2_x86_64"
+
+      desired_size = 1
+      min_size     = 1
+      max_size     = 2
+
+      labels = {
+        workload = "cpu"
+      }
     }
 
-    cpu_group_2 = {
+    gpu_group = {
       instance_types = ["t3.micro"]
-      ami_type = "AL2_x86_64"
-      desired_size   = 1
-      min_size       = 1
-      max_size       = 2
+      ami_type       = "AL2_x86_64"
+
+      desired_size = 1
+      min_size     = 1
+      max_size     = 2
+
+      labels = {
+        workload = "gpu"
+      }
     }
   }
 }
--- eks/variables.tf
+++ eks/variables.tf
@@ -1,11 +1,14 @@
 variable "cluster_name" {
+  type    = string
   default = "demo-eks-cluster"
 }
 
 variable "region" {
+  type    = string
   default = "eu-central-1"
 }
 
 variable "state_bucket" {
+  type    = string
   default = "tf-state-olena-eks-2026"
 }
